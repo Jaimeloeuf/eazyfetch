23073f6e91ed417286ce1e534f94637e45afa4ef
Jaime <jaimeloeuf@gmail.com>
2020-08-01 23:22:42 +0800

    Initial Commit:     Scaffolded project with https://github.com/Enkel-Digital/fetch-with-fire

---
+++ README.md
@@ -0,0 +1,43 @@
+# eazyfetch
+Opinionated library that extends the browsers' fetch API to simplify its API and to provide a simple to use plugin architecture to allow for more advanced features to be easily built on top of the fetch API.
+
+Note that this is essentially a thin wrapper over [fetch](https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API) with some added functionalities to,
+1. Simplify the fetch API through abstractions and opinionated design patterns.
+2. Allow you to define base API URL(s) to make API calls cleaner and simpler, allowing you to change the base URL for all APIs at a single location.
+3. Extend or modify [fetch request's init parameter](https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/fetch#Parameters) by layering multiple init objects easily.
+4. Simple to use plugin architecture to allow advanced features to be added easily.
+
+## Dependencies
+- This package depends on the browsers' fetch method.
+    - *Will throw an error if this library is loaded in an environment where fetch is unavailable!*
+
+## How to use this library
+1. User register plugins that they want to use right after loading this library in a module, and use the newly created object as the single default export of that module.
+2. Every where that needs to call APIs, just import the module that was created in step 1, and use it according to the API
+
+## Examples
+-- TODO --
+
+## Plugin System
+How the plugin system work:
+- Plugins are modules that are used as "middlewares" or simply code that run in sequence for every API call.
+
+Notes / FAQ:
+- Plugins are used on all API calls, meaning that plugins are used for ALL and different base API URLs, so you might want to take this into consideration, especially if you are building authentication plugins. Make sure to wrap these plugins in factory functions to store what base API URLs to run on before registering the plugin to limit its use.
+
+## Plugins available
+- 
+
+## Roadmap / planned features
+- Allow users to define base API URL specific plugins instead of making plugin developers do route checking manually themselves.
+
+
+## License, Author, Contributing and credits
+This project takes inspirations from these projects:
+- <https://github.com/RealmTeam/easyfetch>
+- <https://github.com/Enkel-Digital/fetch-with-fire>
+
+This project is developed and made available under the "MIT License". Feel free to use it however you like and contribute if you would like to help make this a better project!  
+If you have any questions, contact us via [email](mailto:tech@enkeldigital.com)  
+Authors:
+- [JJ](https://github.com/Jaimeloeuf)
---
+++ src/index.js
@@ -0,0 +1,232 @@
+/**
+ * Module that simplifies and extends the browser fetch method with a easier API and plugin architecture
+ * @author JJ
+ */
+
+// Throws an error and prevent module from being used if fetch is not available
+if (!window.fetch) throw new Error("FETCH API NOT AVAILABLE ON BROWSER");
+
+import deepmerge from "deepmerge";
+
+import getParsedResponse from "./getParsedResponse";
+
+/**
+ * Suggestion: import package as "api" to avoid name collision with window.fetch
+ */
+export default class fetch {
+  /**
+   * @param {string} apiUrl Base API URL
+   * @param {function} [errorHandler] Error handling function for when the fetch failed
+   */
+  constructor(firebaseAuth, apiUrl, errorHandler) {
+    if (!firebaseAuth)
+      throw new Error("Firebase Auth is required for this to work");
+
+    // Internal firebase auth instance that must be set to share the same auth instance between this library and your app
+    this._firebaseAuth = firebaseAuth;
+
+    // Set the module's internal base api URL
+    this._apiUrl = apiUrl;
+
+    // Only set the error handler if defined, else keep the default handler
+    if (errorHandler) this._errorHandler = errorHandler;
+    else this._errorHandler = defaultErrorHandler;
+
+    // Empty factory function calls to use default empty object
+    this.get = this._get();
+    this.post = this._post();
+    this.patch = this._patch();
+    this.put = this._put();
+    this.delete = this._delete();
+  }
+
+  /**
+   * USE WITH CAUTION. If you set a new base URL, this will affect every other call out there.
+   * --- Experimental --- Might be removed in future versions. DO NOT depend on this
+   * @param {string} apiUrl Base API URL
+   */
+  __setBaseUrl(apiUrl) {
+    // Set the module's internal base api URL
+    this._apiUrl = apiUrl;
+  }
+
+  /**
+   * Inner fetch function used to prepend API base URL and parse the response
+   * @function _fetch
+   * @param {String} url path of the API only, the base API will be prepended
+   * @param {object} init Request object required by fetch
+   */
+  async _fetch(url = "", init) {
+    try {
+      // Call window fetch with prepended API URL and default request object
+      const response = await window.fetch(this._apiUrl + url, init);
+
+      const parsedResponse = await getParsedResponse(response);
+
+      /* Return base on type of body data and include status code along side */
+      if (parsedResponse.json)
+        return { ...parsedResponse.response, statusCode: response.status };
+      else if (parsedResponse.string)
+        return { body: parsedResponse.response, statusCode: response.status };
+      else throw new Error("Invalid body type. Neither JSON nor String");
+    } catch (error) {
+      return this._errorHandler(error);
+    }
+  }
+
+  /**
+   * GET curried function that takes a init object before an URL
+   */
+  _get(init = {}) {
+    // Arrow function to inherit "this", without using explicit "this" binding
+    return async (url) =>
+      this._fetch(
+        url,
+        deepmerge(
+          {
+            method: "GET",
+            headers: {
+              Authorization: await getAuthHeader(this._firebaseAuth),
+            },
+          },
+          init
+        )
+      );
+  }
+
+  /**
+   * POST curried function that takes a init object before an URL and data
+   */
+  _post(init = {}) {
+    // Arrow function to inherit "this", without using explicit "this" binding
+    return async (url, data) => {
+      if (data) init.body = JSON.stringify(data);
+
+      return this._fetch(
+        url,
+        deepmerge(
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": data ? "application/json" : undefined,
+              Authorization: await getAuthHeader(this._firebaseAuth),
+            },
+          },
+          init
+        )
+      );
+    };
+  }
+
+  /**
+   * PATCH curried function that takes a init object before an URL and data
+   */
+  _patch(init = {}) {
+    // Arrow function to inherit "this", without using explicit "this" binding
+    return async (url, data) => {
+      if (data) init.body = JSON.stringify(data);
+
+      return this._fetch(
+        url,
+        deepmerge(
+          {
+            method: "PATCH",
+            headers: {
+              "Content-Type": data ? "application/json" : undefined,
+              Authorization: await getAuthHeader(this._firebaseAuth),
+            },
+          },
+          init
+        )
+      );
+    };
+  }
+
+  /**
+   * PUT curried function that takes a init object before an URL and data
+   */
+  _put(init = {}) {
+    // Arrow function to inherit "this", without using explicit "this" binding
+    return async (url, data) => {
+      if (data) init.body = JSON.stringify(data);
+
+      return this._fetch(
+        url,
+        deepmerge(
+          {
+            method: "PUT",
+            headers: {
+              "Content-Type": data ? "application/json" : undefined,
+              Authorization: await getAuthHeader(this._firebaseAuth),
+            },
+          },
+          init
+        )
+      );
+    };
+  }
+
+  /**
+   * DELETE curried function that takes a init object before an URL and data
+   * It is not recommended to include a request message body even though you are able to
+   */
+  _delete(init = {}) {
+    // Arrow function to inherit "this", without using explicit "this" binding
+    return async (url, data) => {
+      if (data) init.body = JSON.stringify(data);
+
+      return this._fetch(
+        url,
+        deepmerge(
+          {
+            method: "DELETE",
+            headers: {
+              "Content-Type": data ? "application/json" : undefined,
+              Authorization: await getAuthHeader(this._firebaseAuth),
+            },
+          },
+          init
+        )
+      );
+    };
+  }
+
+  /**
+   * Function to modify init object only once before making a new request
+   * @param {object} init Request object for fetch
+   * @returns {object} Same API object with custom request object partially applied.
+   *
+   * @example
+   * api.modify(custom request object).post(url, data)
+   */
+  modify(init) {
+    // Return the http methods to chain it and make a request
+    return {
+      get: this._get(init),
+      post: this._post(init),
+      patch: this._get(init),
+      put: this._post(init),
+      delete: this._post(init),
+    };
+  }
+
+  /**
+   * USE WITH CAUTION
+   * Function to modify init objects permanently for ALL methods
+   * @param {object} init Request object for fetch
+   * @returns {object} Same object with updated methods with new custom request object partially applied.
+   *
+   * @example
+   * api.modifyPermanently(custom request object).post(url, data)
+   */
+  modifyPermanently(init) {
+    this.get = this._get(init);
+    this.post = this._post(init);
+    this.patch = this._get(init);
+    this.put = this._post(init);
+    this.delete = this._post(init);
+
+    // Return object to allow caller to chain this method
+    return this;
+  }
+}
